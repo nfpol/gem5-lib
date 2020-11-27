f489ff08e1a3d37dc5a564ec5d5da55c47fa079d
nfpol <nikolaos.polychronou@cea.fr>
2020-11-27 17:38:51 +0100

    monitor

---
+++ monitor-asm/random.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>   //for sleep()
+
+
+int main()
+{		
+		for(int i =0; i<15; i++) {
+			
+			/* resetstats */
+  			__asm__ __volatile__ ("mov x0, #0; mov x1, #0; .inst 0XFF000110 | (0x40 << 16);");
+			sleep(0.1); //sleep 0.1s
+			/* dumpstats */
+	     		__asm__ __volatile__ ("mov x0, #0; mov x1, #0; .inst 0xFF000110 | (0x41 << 16);");
+			
+		}
+    
+    return 0;
+}
