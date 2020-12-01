aabdf484363076ed5cdc8b674efc27de8c46f910
Nikolaos  Polychronou <nfpolychronou@hotmail.com>
2020-12-01 16:48:38 +0100

    a

--- random-bench-attack-monitor/reg/random.c
+++ random-bench-attack-monitor/reg/random.c
@@ -16,19 +16,20 @@ int main()
 {
     		int randomnumber;
 		srand(time(NULL));
-		FILE * fPtr;
-  		fPtr = fopen("./monitor/output-reg.dat", "w");
-  		if(fPtr == NULL){
+		//FILE * fPtr;
+  		//fPtr = fopen("./monitor/output-reg.dat", "w");
+  		//if(fPtr == NULL){
        		/* File not created hence exit */
-        		printf("Unable to create file.\n");
-        		exit(EXIT_FAILURE);
-		}
+        		//printf("Unable to create file.\n");
+        		//exit(EXIT_FAILURE);
+		//}
 		
 		for(int i =0; i<15; i++) {
 			randomnumber = rand() % 4+ 1;
 			if (randomnumber==1){
 					/*Run libflush example */
-				fprintf(fPtr, "libflush\n");
+				//fprintf(fPtr, "libflush\n");
+				printf("libflush\n");
 				chdir("/home/nikos/armageddon/libflush/"); 
 				//system("m5 resetstats");
 				system("./example/build/armv8/release/bin/example -s 400 -n  1000 -x 1 -z 10");
@@ -36,7 +37,8 @@ int main()
 				chdir("/home/nikos/gem5-lib/");	
 			}
 			else if (randomnumber==2){
-				fprintf(fPtr, "basicmath\n");
+				//fprintf(fPtr, "basicmath\n");
+				printf("basicmath\n");
 				chdir("/home/nikos/gem5-lib/automotive/basicmath"); 
 				//system("m5 resetstats");
 				system("./basicmath_small > output_small.txt");
@@ -44,7 +46,8 @@ int main()
 				chdir("/home/nikos/gem5-lib/");
 			}	
 			else if (randomnumber==3){
-				fprintf(fPtr, "bitcount\n");
+				printf(""bitcount\n");
+				//fprintf(fPtr, "bitcount\n");
 				chdir("/home/nikos/gem5-lib/automotive/bitcount"); 
 				//system("m5 resetstats");
 				system("./bitcnts 75000 > output_small.txt");
@@ -52,7 +55,8 @@ int main()
 				chdir("/home/nikos/gem5-lib/");	
 			}
 			else if (randomnumber==4){
-				fprintf(fPtr, "sha\n");
+				printf("sha\n");
+				//fprintf(fPtr, "sha\n");
 				chdir("/home/nikos/gem5-lib/security/sha"); 
 				//system("m5 resetstats");
 				system("./sha input_small.asc > output_small.txt");
@@ -60,11 +64,13 @@ int main()
 				chdir("/home/nikos/gem5-lib/");	
 			}
 			else {
-				fprintf(fPtr, "nothing");
+				//fprintf(fPtr, "nothing");
+				printf("nothing");
 			}
 			
 		}
-	fprintf(fPtr, "closing random execution\n");
-	fclose(fPtr);
+	//fprintf(fPtr, "closing random execution\n");
+	printf("closing random execution\n");
+	//fclose(fPtr);
 	return 0;
 }
