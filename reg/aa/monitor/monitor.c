596d49ca66bbbb45037a4fa8c011969ef732510d
nfpol <nikolaos.polychronou@cea.fr>
2020-12-02 16:24:56 +0100

    a

---
+++ reg/aa/monitor/monitor.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <linux/perf_event.h>
+#include "arm_v8.h"
+
+#include <unistd.h>   //for sleep()
+
+extern void init_pmu(void);
+extern void select_event(void);
+extern void reset_event_counters(void);
+extern void reset_cycle_counter(void);
+extern void event_counters_disable(void);
+extern void cycle_counter_disable(void);
+
+             
+int main(int argc, char* argv[]) {
+ /* Define parameters */
+	double timing_frame = 0;
+	int loop = 0;
+	FILE* logfile = NULL;
+  int i,j,k = 0;
+  FILE * fPtr;
+  fPtr = fopen("output.dat", "w");
+  if(fPtr == NULL){
+        /* File not created hence exit */
+        printf("Unable to create file.\n");
+        exit(EXIT_FAILURE);
+  }
+	
+	/* Parse arguments */
+  static const char* short_options = "t:lm";
+	
+	int c;
+  while ((c = getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
+		switch (c) {
+      case 't':
+        timing_frame = atof(optarg);
+        if (timing_frame <= 0) {
+          fprintf(stderr, "Error: timing frame is negative.\n");
+          return -1;
+        }
+        break;
+      case 'lm':
+        loop = atoi(optarg);
+        if (loop <= 0) {
+          fprintf(stderr, "Error: loop is negative\n");
+          return -1;
+        }
+        break;
+				/*
+      case 'h':
+        print_help(argv);
+        return 0;
+      case '?':
+      default:
+        fprintf(stderr, "Error: Invalid option '-%c'\n", optopt);
+        return -1; */
+    }
+  }
+		
+	init_pmu();
+	select_event();
+	reset_event_counters();
+	reset_cycle_counter();
+	
+	printf("Performance monitor results\n");
+	fprintf(fPtr, "i cache refills---|---retired branches---|---d cache refills---|---branch predictor misses---|---predictable branch speculatively executed---|---CPU cycles event counter---|---CPU cycles ccnt\n");
+	while(i < loop){
+		reset_event_counters(); //reset event counters
+		pmu_reset_cycle_counter();
+		sleep(timing_frame);  //sleep 10ms
+		//sleep(1);
+		event_counters_disable();
+		cycle_counter_disable();
+		fprintf(fPtr, "%u                            ", get_event_counter(0));
+		fprintf(fPtr, "%u                            ", get_event_counter(1) );
+		fprintf(fPtr, "%u                            ", get_event_counter(2) );
+		fprintf(fPtr, "%u                            ", get_event_counter(3) );
+		fprintf(fPtr, "%u                            ", get_event_counter(4) );
+		fprintf(fPtr, "%u                            ", get_event_counter(5) );
+		fprintf(fPtr, "%u                            ", get_event_counter(6) );
+		fprintf(fPtr, "%lu                            \n", get_timing());
+		i++;
+		pmu_enable_config_counter(0);
+		pmu_enable_config_counter(1);
+		pmu_enable_config_counter(2);
+		pmu_enable_config_counter(3);
+		pmu_enable_config_counter(4);
+		pmu_enable_config_counter(5);
+		pmu_enable_config_counter(6);
+		pmu_enable_all_counters();
+	}
+	
+
+	printf("PMU monitor is closing ....\n");
+	fclose(fPtr);
+	
+		
+  exit(0);
+  
+
+}
---
+++ reg/aa/random.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <linux/perf_event.h>
+
+
+#include <unistd.h>   //for sleep()
+
+
+
+int main(int argc, char* argv[])
+{
+		double timing_frame = 0;
+		int loop_monitor = 0;
+		int loop_rand = 0;
+		FILE* logfile = NULL;
+    int randomnumber;
+		srand(time(NULL));
+		FILE * fPtr;
+  		fPtr = fopen("./monitor/output-reg.dat", "a");
+  		if(fPtr == NULL){
+       		/* File not created hence exit */
+        		printf("Unable to create file.\n");
+        		exit(EXIT_FAILURE);
+		}
+		
+		
+			/* Parse arguments */
+		static const char* short_options = "t:lm:lr";
+		/*static struct option long_options[] = {
+			{"timing",           required_argument, NULL, 't'},
+			{"loop_monitor",     required_argument, NULL, 'lm'},
+			{"loop_rand",        required_argument, NULL, 'lr'},
+			{ NULL,             0, NULL, 0}
+		};*/
+	
+		int c;
+		while ((c = getopt_long(argc, argv, short_options, NULL)) != -1) {
+			switch (c) {
+				case 't':
+					timing_frame = atof(optarg);
+					if (timing_frame <= 0) {
+						fprintf(stderr, "Error: timing frame is negative.\n");
+						return -1;
+					}
+					break;
+				case 'lm':
+					loop_monitor = atoi(optarg);
+					if (loop_monitor <= 0) {
+						fprintf(stderr, "Error: loop_monitor is negative\n");
+						return -1;
+					}
+					break;
+				case 'lr':
+					loop_rand = atoi(optarg);
+					if (loop_rand <= 0) {
+						fprintf(stderr, "Error: loop_rand is negative\n");
+						return -1;
+					}
+					break;
+					/*
+				case 'h':
+					print_help(argv);
+					return 0;
+				case '?':
+				default:
+					fprintf(stderr, "Error: Invalid option '-%c'\n", optopt);
+					return -1; */
+			}
+		}
+		system("./monitor/monitor -lm loop_monitor -t timing_frame &");
+		for(int i =0; i<loop_rand; i++) {
+			printf("nikos");
+			randomnumber = rand() % 4+ 1;
+			if (randomnumber==1){
+					/*Run libflush example */
+				fprintf(fPtr, "libflush\n");
+				//printf("libflush\n");
+				chdir("/home/nikos/armageddon/libflush/"); 
+				//system("m5 resetstats");
+				system("./example/build/armv8/release/bin/example -s 400 -n  1000 -x 1 -z 10");
+				//system("m5 dumpstats");
+				chdir("/home/nikos/gem5-lib/");	
+			}
+			else if (randomnumber==2){
+				fprintf(fPtr, "basicmath\n");
+				//printf("basicmath\n");
+				chdir("/home/nikos/gem5-lib/automotive/basicmath"); 
+				//system("m5 resetstats");
+				system("./basicmath_small > output_small.txt");
+				//system("m5 dumpstats");
+				chdir("/home/nikos/gem5-lib/");
+			}	
+			else if (randomnumber==3){
+				//printf(""bitcount\n");
+				fprintf(fPtr, "bitcount\n");
+				chdir("/home/nikos/gem5-lib/automotive/bitcount"); 
+				//system("m5 resetstats");
+				system("./bitcnts 75000 > output_small.txt");
+				//system("m5 dumpstats");
+				chdir("/home/nikos/gem5-lib/");	
+			}
+			else if (randomnumber==4){
+				//printf("sha\n");
+				fprintf(fPtr, "sha\n");
+				chdir("/home/nikos/gem5-lib/security/sha"); 
+				//system("m5 resetstats");
+				system("./sha input_small.asc > output_small.txt");
+				//system("m5 dumpstats");
+				chdir("/home/nikos/gem5-lib/");	
+			}
+			else {
+				fprintf(fPtr, "nothing");
+				//printf("nothing");
+			}
+			
+		}
+	fprintf(fPtr, "closing random execution\n");
+	//printf("closing random execution\n");
+	fclose(fPtr);
+	return 0;
+}
