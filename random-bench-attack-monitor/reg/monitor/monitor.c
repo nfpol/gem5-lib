9beeed3c865389a1e87c6faa043b2a12a09044dd
nfpol <nikolaos.polychronou@cea.fr>
2020-12-02 12:13:06 +0100

    a

--- random-bench-attack-monitor/reg/monitor/monitor.c
+++ random-bench-attack-monitor/reg/monitor/monitor.c
@@ -16,7 +16,8 @@ extern void init_pmu(void);
 extern void select_event(void);
 extern void reset_event_counters(void);
 extern void reset_cycle_counter(void);
-
+extern void event_counters_disable(void);
+extern void cycle_counter_disable(void);
              
 int main(void) {
   int i,j,k = 0;
