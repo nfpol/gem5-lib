65098e6ce36752eb6064be2b2df661a3f00bafbc
nfpol <nikolaos.polychronou@cea.fr>
2020-12-03 10:42:32 +0100

    aaa

--- reg/aa/random.c
+++ reg/aa/random.c
@@ -139,7 +139,7 @@ int main(int argc, char* argv[])
 		}
 	fprintf(fPtr, "closing random execution\n");
 	//printf("closing random execution\n");
-	system("pkill -f monitor");
+	//system("pkill -f monitor");
 	fclose(fPtr);
 	return 0;
 }
