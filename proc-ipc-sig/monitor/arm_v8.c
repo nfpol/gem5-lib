f574a3e64536b8f8977923ec3326dd16b41f330d
nfpol <nikolaos.polychronou@cea.fr>
2020-12-11 13:53:41 +0100

    monitor

---
+++ proc-ipc-sig/monitor/arm_v8.c
@@ -0,0 +1,81 @@
+//#include <linux/module.h>       /* Needed by all modules */
+#include <linux/kernel.h>       /* Needed for KERN_ERR */
+#include <linux/fs.h>
+#include <linux/wait.h>
+#include "arm_v8.h"
+#include <stdio.h>
+#include<stdlib.h>
+#include <unistd.h>
+
+
+unsigned seconds=5;
+
+void init_pmu(void)
+{
+	pmu_el0_access();  // Allow access to PMU from User Mode--- works only in kernel mode
+	pmu_enable_el0();  // Enable the PMU in EL0
+}
+
+void select_event(void)
+{
+	/*  GEM5 events are different than arm documented   ------->    look in gem5/arch/arm/ArmPMU*/
+	pmu_event_config(0, 0x02);  // i cache refills
+	asm volatile("ISB"); 
+	pmu_event_config(1, 0x21);  // retired branches
+	asm volatile("ISB"); 
+	pmu_event_config(2, 0x05);  // d cache refills
+	asm volatile("ISB"); 
+ 	pmu_event_config(3, 0x08);  // retired instructions
+ 	asm volatile("ISB"); 
+	pmu_event_config(4, 0x10);  // branch predictor misses
+	asm volatile("ISB"); 
+	pmu_event_config(5, 0x12);  // Predictable branch speculatively executed
+	asm volatile("ISB"); 
+	pmu_event_config(6, 0x11);  // CPU cycles
+	asm volatile("ISB"); 
+	pmu_enable_config_counter(0);
+	asm volatile("ISB"); 
+	pmu_enable_config_counter(1);
+	asm volatile("ISB"); 
+	pmu_enable_config_counter(2);
+	asm volatile("ISB"); 
+	pmu_enable_config_counter(3);
+	asm volatile("ISB"); 
+	pmu_enable_config_counter(4);
+	asm volatile("ISB"); 
+	pmu_enable_config_counter(5);
+	asm volatile("ISB"); 
+	pmu_enable_config_counter(6);
+	asm volatile("ISB"); 
+}
+
+void cycle_counter_disable(void){
+	pmu_cycle_counter_disable();  // Stop CCNT cycle counter 
+}
+
+void event_counters_disable(void){
+	pmu_event_counters_disable(0);   // Stop event counter 0
+	pmu_event_counters_disable(1);   // Stop event counter 1
+	pmu_event_counters_disable(2);   // Stop event counter 2
+	pmu_event_counters_disable(3);   // Stop event counter 3
+	pmu_event_counters_disable(4);   // Stop event counter 4
+	pmu_event_counters_disable(5);   // Stop event counter 5
+	pmu_event_counters_disable(5);   // Stop event counter 6
+}
+
+void reset_event_counters(void)
+{	
+	pmu_reset_event_counters_all();  // Reset the configurable counters
+}
+
+void reset_cycle_counter(void)
+{	
+	pmu_reset_cycle_counter();  // Reset the CCNT (cycle counter)
+}
+
+
+
+
+
+
+
---
+++ proc-ipc-sig/random.c
@@ -0,0 +1,238 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <linux/perf_event.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+#include "ipc.h"
+#include "./monitor/arm_v8.h"
+
+#include <unistd.h>   //for sleep()
+
+/* IPC */
+static int ipc_fd;
+static void *addr;
+static struct shm_msg *client_msg;
+static struct shm_msg *server_msg;
+
+static void print_help(char* argv[]) {
+  fprintf(stdout, "Usage: %s [OPTIONS]\n", argv[0]);
+  fprintf(stdout, "\t-t, -timing_frame <value>\t TIMING FRAME (default: t 1  --> 1/10 --> 0.1sec )\n");
+  fprintf(stdout, "\t-m, -loop_monitor <value>\t  Loop for the moniotr (default:  m 1000)\n");
+  fprintf(stdout, "\t-r, -loop_rand <value>\t How many random choices of benchmark attack to execute  (default: -r 20)\n");
+  fprintf(stdout, "\t-d, -div <value>\t time divider (default:  div 10)\n");
+  fprintf(stdout, "\t-h, -help\t\t Help page\n");
+}
+
+static void random_execution(void){
+	randomnumber = rand() % 4+ 1;
+	while(1) {
+		if(client_msg->status == 1) {
+			server_msg->status = 0;
+			memcpy(msg, client_msg->msg, client_msg->len);
+			//fprintf(fPtr, "%s", client_msg->msg);
+			fprintf(fPtr, "%s", msg);
+			client_msg->status = 0;
+			server_msg->status = 1;
+			break;
+		}
+	}
+	if (randomnumber==1){
+		pmu_cycle_counter_disable();
+		pmu_event_counters_disable_all();
+		fprintf(fPtr, "libflush ...\n");
+		asm volatile("ISB");
+		pmu_enable_cycle_counter();
+		pmu_enable_all_counters();
+		chdir("/home/nikos/armageddon/libflush/"); 
+		system("./example/build/armv8/release/bin/example -s 400 -n  1000 -x 1 -z 10");
+		chdir("/home/nikos/gem5-lib/");	
+	}
+	else if (randomnumber==2){
+		pmu_cycle_counter_disable();
+		pmu_event_counters_disable_all();
+		fprintf(fPtr, "basicmath small ...\n");
+		asm volatile("ISB");
+		pmu_enable_cycle_counter();
+		pmu_enable_all_counters();
+		chdir("/home/nikos/gem5-lib/automotive/basicmath");
+		system("./basicmath_small > output_small.txt");
+		chdir("/home/nikos/gem5-lib/");
+	}	
+	else if (randomnumber==3){
+		pmu_cycle_counter_disable();
+		pmu_event_counters_disable_all();
+		fprintf(fPtr, "bitcount small ...\n");
+		asm volatile("ISB");
+		pmu_enable_cycle_counter();
+		pmu_enable_all_counters();
+		chdir("/home/nikos/gem5-lib/automotive/bitcount"); 
+		system("./bitcnts 75000 > output_small.txt");
+		chdir("/home/nikos/gem5-lib/");	
+	}
+	else if (randomnumber==4){
+		pmu_cycle_counter_disable();
+		pmu_event_counters_disable_all();
+		fprintf(fPtr, "sha small ...\n");
+		asm volatile("ISB");
+		pmu_enable_cycle_counter();
+		pmu_enable_all_counters();
+		chdir("/home/nikos/gem5-lib/security/sha"); 
+		system("./sha input_small.asc > output_small.txt");
+		chdir("/home/nikos/gem5-lib/");	
+	}
+	else {
+		fprintf(fPtr, "wrong rand ...\n");
+	}
+				
+
+}
+
+int main(int argc, char* argv[])
+{
+		char command[100];
+		int div = 10;
+		int timing_frame = 1;
+		int loop_monitor = 1000;
+		int loop_rand = 20;
+		FILE* logfile = NULL;
+    int randomnumber;
+		srand(time(NULL));
+
+		char* data = calloc(32, sizeof(data));
+		system("./delete-out");	 
+		/* Parse arguments */
+		static const char* short_options = "t:m:r:d:h:";
+		
+		int c;
+		while ((c = getopt_long(argc, argv, short_options, NULL)) != -1) {
+			switch (c) {
+				case 't':
+					timing_frame = atoi(optarg);
+					if (timing_frame <= 0) {
+						fprintf(stderr, "Error: timing frame is negative.\n");
+						return -1;
+					}
+					break;
+				case 'm':
+					loop_monitor = atoi(optarg);
+					if (loop_monitor <= 0) {
+						fprintf(stderr, "Error: loop_monitor is negative\n");
+						return -1;
+					}
+					break;
+				case 'r':
+					loop_rand = atoi(optarg);
+					if (loop_rand <= 0) {
+						fprintf(stderr, "Error: loop_rand is negative\n");
+						return -1;
+					}
+					break;
+				case 'd':
+					div = atoi(optarg);
+					if (div <= 0) {
+						fprintf(stderr, "Error: loop_rand is negative\n");
+						return -1;
+					}
+					break;
+				case 'h':
+					print_help(argv);
+					return 0;
+				case '?':
+				default:
+					fprintf(stderr, "Error: Invalid option '-%c'\n", optopt);
+					return -1; 
+			}
+		}
+		
+		FILE * fPtr;
+		fPtr = fopen("./output.dat", "w");
+		asm volatile ("ISB");
+		if(fPtr == NULL){
+			/* File not created hence exit */
+			printf("Unable to create file.\n");
+			exit(EXIT_FAILURE);
+		}	
+		/* create shm */
+		  if((server_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, PERM_FILE)) == -1) {
+		      printf("shm_open error : %s\n", strerror(errno));
+		      return -1;
+		  }
+		
+		/* set size */
+		  if(ftruncate(server_fd, MSG_SIZE_MAX) == -1) {
+		      printf("ftruncate error : %s\n", strerror(errno));
+		      goto out;
+		  }
+		
+		/* mmap */
+		  addr = mmap(NULL, MSG_SIZE_MAX, PROT_READ | PROT_WRITE, MAP_SHARED, server_fd, 0);
+		  if(addr == MAP_FAILED) {
+		      printf("mmap error : %s\n", strerror(errno));
+		      goto out;
+		  }
+		  memset(addr, 0, MSG_SIZE_MAX);
+		
+		client_msg = (struct shm_msg *)((char*)addr + HM_CLIENT_BUF_IDX_MONITOR);
+		server_msg = (struct shm_msg *)((char*)addr + SHM_SERVER_BUF_IDX);
+		server_msg->status = 1;
+		server_msg->wait = 1;
+		server_msg->finish = 0;
+		client_msg->status = 0;
+		client_msg->wait = 1;
+		client_msg->finish = 0;
+			
+		printf("server is running...\n");
+		
+		asm volatile ("ISB");
+		server_msg->wait = 0;
+		sprintf(command, "nice --2 ./monitor/monitor -m %u -t %u -d %u &", loop_monitor, timing_frame, div);
+		system(command);
+		
+		while(1){
+			if(client_msg->status == 1 && client_msg->wait == 0) break;
+		}
+		
+		asm volatile ("ISB");
+		fprintf(fPtr, "random execution started ...\n");
+		for(int i =0; i<loop_rand; i++) {
+			random_execution();
+		}
+		
+		server_msg->finish = 1;
+		
+		while(1){
+			if(client_msg->finish == 1) break;
+		}
+		//maybe it will work as when random execution stop we dont care
+		pmu_cycle_counter_disable();
+		pmu_event_counters_disable_all();
+		fprintf(fPtr, "closing random execution ...\n");
+		//system("pkill -f monitor");
+		
+out:
+		/* destroy shm */
+    if(munmap(addr, MSG_SIZE_MAX) == -1) {
+        printf("munmap error : %s\n", strerror(errno));
+    }
+		if(close(server_fd) == -1) {
+        printf("close error : %s\n", strerror(errno));
+    }
+		if(shm_unlink(SHM_NAME) == -1) {
+        printf("shm_unlink error : %s\n", strerror(errno));
+    }
+		
+		
+		fclose(fPtr);
+		return 0;
+}
